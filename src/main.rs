2615345439e4a45c80fd8ff9118160a0b98e0ba1
guemax <github-mg@guenther.cloud>
2023-08-05 14:34:06 +0200

    Implement logging
    Closes #1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use log::{debug, error, info, trace, warn};
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    trace!("So is there someone?");
+    debug!("Test! 1, 2, 3, Test!");
+    info!("Hello World!");
+    warn!("What was that?!?");
+    error!("Aaaaaaaarrgrgghhhhhh!");
 }
