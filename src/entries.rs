e48e71a7504e623de0eca3beb78a0c3eae875dfb
guemax <github-mg@guenther.cloud>
2023-08-07 22:35:36 +0200

    Add `CsvFileManipulator` for testing purposes

--- src/entries.rs
+++ src/entries.rs
@@ -20,6 +20,12 @@ impl Entries {
 
 #[cfg(test)]
 mod test {
+    use std::{
+        fs::File,
+        io::prelude::*,
+        path::{Display, Path},
+    };
+
     use super::*;
     use parameterized::parameterized;
 
@@ -41,5 +47,71 @@ mod test {
     fn test_getting_number_of_entries(filename: &str, expected_number_of_entries: i32) {
         let entries = Entries::load(filename);
         assert_eq!(expected_number_of_entries, entries.number());
+
+        let mut manipulator = CsvFileManipulator::new(filename);
+
+        manipulator.backup_csv_file();
+        manipulator.add_new_entry();
+
+        let entries = Entries::load(filename);
+        assert_eq!(expected_number_of_entries + 1, entries.number());
+    }
+
+    struct CsvFileManipulator<'a> {
+        path: &'a Path,
+        display: Display<'a>,
+        buffer: String,
+    }
+
+    impl CsvFileManipulator<'_> {
+        pub fn new(filename: &'static str) -> Self {
+            Self {
+                path: Path::new(filename),
+                display: Path::new(filename).display(),
+                buffer: "".to_string(),
+            }
+        }
+        pub fn backup_csv_file(&mut self) {
+            let mut file = match File::open(self.path) {
+                Err(why) => panic!("Unable to open file {}: {}", self.display, why),
+                Ok(file) => file,
+            };
+
+            let mut buffer = String::new();
+            match file.read_to_string(&mut buffer) {
+                Err(why) => panic!("Unable to read file {}: {}", self.display, why),
+                Ok(_) => (),
+            }
+            self.buffer = buffer;
+        }
+
+        pub fn add_new_entry(&mut self) {
+            let mut file = match File::create(self.path) {
+                Err(why) => panic!("Unable to open file {}: {}", self.display, why),
+                Ok(file) => file,
+            };
+
+            let buffer_with_new_entry =
+                self.buffer.to_owned() + "\n2023-08-09T12:00:00Z,2023-08-09T15:00:00Z,";
+
+            match file.write_all(buffer_with_new_entry.as_bytes()) {
+                Err(why) => panic!("Unable to write to file {}: {}", self.display, why),
+                Ok(_) => (),
+            }
+        }
+    }
+
+    impl Drop for CsvFileManipulator<'_> {
+        fn drop(&mut self) {
+            let mut file = match File::create(self.path) {
+                Err(why) => panic!("Unable to create file {}: {}", self.display, why),
+                Ok(file) => file,
+            };
+
+            match file.write_all(self.buffer.as_bytes()) {
+                Err(why) => panic!("Unable to write to file {}: {}", self.display, why),
+                Ok(_) => (),
+            }
+        }
     }
 }
