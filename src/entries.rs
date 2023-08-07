2aadd870b95b11a90820c0c1600bb5d442fa3fa9
guemax <github-mg@guenther.cloud>
2023-08-07 15:17:04 +0200

    Use crate `parameterized` for expanding tests

--- src/entries.rs
+++ src/entries.rs
@@ -13,12 +13,25 @@ impl Entries {
 #[cfg(test)]
 mod test {
     use super::*;
+    use parameterized::parameterized;
 
     const DF_WITH_ONE_ENTRY: &str = "test/df_with_one_entry.csv";
+    const DF_WITH_TWO_ENTRIES: &str = "test/df_with_two_entries.csv";
 
-    #[test]
-    fn test_loading_entries() {
-        let entries = Entries::load(DF_WITH_ONE_ENTRY);
-        assert_eq!(1, entries.number());
+    #[parameterized(filename = {
+        DF_WITH_ONE_ENTRY, DF_WITH_TWO_ENTRIES
+    })]
+    fn test_loading_entries(filename: &str) {
+        let _ = Entries::load(filename);
+    }
+
+    #[parameterized(filename = {
+        DF_WITH_ONE_ENTRY, DF_WITH_TWO_ENTRIES
+    }, expected_number_of_entries = {
+        1, 2
+    })]
+    fn test_getting_number_of_entries(filename: &str, expected_number_of_entries: i32) {
+        let entries = Entries::load(filename);
+        assert_eq!(expected_number_of_entries, entries.number());
     }
 }
