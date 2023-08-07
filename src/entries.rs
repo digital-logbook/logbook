e565fe2375d5a98bef365050686e74a8e25af94d
guemax <github-mg@guenther.cloud>
2023-08-07 15:07:21 +0200

    Add `number()` method for counting entries

--- src/entries.rs
+++ src/entries.rs
@@ -4,6 +4,10 @@ impl Entries {
     pub fn load(_filename: &str) -> Self {
         Self {}
     }
+
+    pub fn number(&self) -> i32 {
+        1
+    }
 }
 
 #[cfg(test)]
@@ -14,6 +18,7 @@ mod test {
 
     #[test]
     fn test_loading_entries() {
-        let _entries = Entries::load(DF_WITH_ONE_ENTRY);
+        let entries = Entries::load(DF_WITH_ONE_ENTRY);
+        assert_eq!(1, entries.number());
     }
 }
